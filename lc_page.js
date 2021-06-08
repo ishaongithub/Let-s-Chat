da5ab357852a50c32fb4140b5816829ba4aa8c78
ishaongithub <75757332+ishaongithub@users.noreply.github.com>
2021-06-08 13:32:57 -0700

    Add files via upload

--- lc_page.js
+++ lc_page.js
@@ -47,8 +47,20 @@ like : updated_likes
 
 }
 
+function send()
+{
+  msg = document.getElementById("msg").value;
+  firebase.database().ref(room_name).push({
+    name:user_name,
+    message:msg,
+    like:0
+   });
+
+  document.getElementById("msg").value = "";
+}
+
 function logout() {
 localStorage.removeItem("user_name");
 localStorage.removeItem("room_name");
-window.location.replace("kwitter.html");
+window.location.replace("index.html");
 }
